7f5a87a176337b76abf9a60dbd3db3c1313239a1
Dario <dariosans@gmail.com>
2025-04-12 21:31:04 -0400

    Implemented jsr.

--- src/instructions.rs
+++ src/instructions.rs
@@ -19,4 +19,19 @@ impl Cpu {
         let hi = self.memory[0xFFFF] as u16;
         self.pc = (hi << 8) | lo;
     }
+
+    /// JSR - Jump to Subroutine
+    pub fn jsr(&mut self) {
+        // Fetch target address from instruction stream
+        let lo = self.memory[(self.pc + 1) as usize] as u16;
+        let hi = self.memory[(self.pc + 2) as usize] as u16;
+        let target = (hi << 8) | lo;
+
+        // [TODO] Might increment PC before calling this, use `.wrapping_add(1)` instead
+        let return_addr = self.pc.wrapping_add(2);
+        self.push((return_addr >> 8) as u8); // Push high byte
+        self.push((return_addr & 0xFF) as u8); // Push low byte
+
+        self.pc = target;
+    }
 }
