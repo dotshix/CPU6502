43f8b654e23804d3c09efa81f8e2e4e6b52ac0f4
Dario <dariosans@gmail.com>
2025-04-17 15:47:56 -0400

    Implemented ROL instruction(acc and mem).

--- src/instructions.rs
+++ src/instructions.rs
@@ -441,14 +441,48 @@ impl Cpu {
         self.memory[self.addr_abs as usize] = value;
 
         // Step 2: Perform shift
-        let result = value << 1;
+        let res = value << 1;
 
         // Step 3: Set flags
         self.set_flag(Flag::Carry, value & 0x80 != 0);
+        self.set_flag(Flag::Zero, res == 0);
+        self.set_flag(Flag::Negative, res & 0x80 != 0);
+
+        // Step 4: Write result
+        self.memory[self.addr_abs as usize] = res;
+    }
+
+    /// ROL - Rotate Left (Memory)
+    pub fn rol_mem(&mut self) {
+        let value = self.memory[self.addr_abs as usize];
+
+        // [Read-Modify-Write] Write original value back
+        self.memory[self.addr_abs as usize] = value;
+
+        let carry_flag = if self.get_flag(Flag::Carry) { 1 } else { 0 };
+
+        // Step 2: Perform shift
+        let res = value << 1 | carry_flag;
+
+        // Step 3: Set flags
+        self.set_flag(Flag::Carry, value & 0x80 != 0);
+        self.set_flag(Flag::Zero, res == 0);
+        self.set_flag(Flag::Negative, res & 0x80 != 0);
+
+        // Step 4: Write result
+        self.memory[self.addr_abs as usize] = res;
+    }
+
+    /// ROL - Rotate Left (Accumulator)
+    pub fn rol_acc(&mut self) {
+        let carry_in = if self.get_flag(Flag::Carry) { 1 } else { 0 };
+        let old_a = self.a;
+        let result = (old_a << 1) | carry_in;
+
+        self.set_flag(Flag::Carry, old_a & 0x80 != 0);
         self.set_flag(Flag::Zero, result == 0);
         self.set_flag(Flag::Negative, result & 0x80 != 0);
 
-        // Step 4: Write result
-        self.memory[self.addr_abs as usize] = result;
+        self.a = result;
     }
 }
