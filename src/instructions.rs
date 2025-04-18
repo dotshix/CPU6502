71b8cb32fee239ba506bd776e39d028b60bafd2a
Dario <dariosans@gmail.com>
2025-04-18 03:02:47 -0400

    Changed bit function to return nothing

--- src/instructions.rs
+++ src/instructions.rs
@@ -109,7 +109,7 @@ impl Cpu {
     }
 
     /// BIT - Bit Test
-    pub fn bit(&mut self) -> u8 {
+    pub fn bit(&mut self) {
         self.fetch();
 
         let result = self.a & self.fetched;
@@ -117,9 +117,8 @@ impl Cpu {
         self.set_flag(Flag::Zero, result == 0); // A & M == 0?
         self.set_flag(Flag::Overflow, self.fetched & 0x40 != 0); // Bit 6 of M
         self.set_flag(Flag::Negative, self.fetched & 0x80 != 0); // Bit 7 of M
-
-        0
     }
+
     /// STY - Store Y
     pub fn sty(&mut self) {
         self.memory[self.addr_abs as usize] = self.y;
